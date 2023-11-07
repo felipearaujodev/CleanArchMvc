e43433843cb2be011245bfdcdf9cad9c0832be1f
Felipe Araujo <comunicar.felipearaujo@gmail.com>
2023-11-07 17:12:39 -0300

    criando testes de unidade para as entidades

---
+++ CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,97 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Validation;
+using FluentAssertions;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class ProductUnitTest1
+    {
+        [Fact]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 99.99m, 1, "Image");
+            action.Should()
+                .NotThrow<DomainExceptionValidation>();
+
+        }
+
+        [Fact]
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Product(-1, "Product Name", "Product Description", 99.99m, 1, "Image");
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid Id value");
+
+        }
+
+        [Fact]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Pr", "Product Description", 99.99m, 1, "Image");
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters");
+
+        }
+
+        [Fact]
+        public void CreateProduct_WithNullNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, null, "Product Description", 99.99m, 1, "Image");
+            action.Should()
+                .Throw<DomainExceptionValidation>();
+
+        }
+
+        [Fact]
+        public void CreateProduct_ShortDescriptionValue_DomainExceptionShortDescription()
+        {
+            Action action = () => new Product(1, "Product Name", "Prod", 99.99m, 1, "Image");
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid description, too short, minimum 5 characters");
+
+        }
+
+        [Fact]
+        public void CreateProduct_WithNullDescriptionValue_DomainExceptionShortDescription()
+        {
+            Action action = () => new Product(1, "Product Name", null, 99.99m, 1, "Image");
+            action.Should()
+                .Throw<DomainExceptionValidation>();
+
+        }
+
+        [Fact]
+        public void CreateProduct_NegativePriceValue_DomainExceptionInvalidPrice()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", -99.99m, 1, "Image");
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid price. Price is required");
+
+        }
+
+        [Theory]
+        [InlineData(-5)]
+        public void CreateProduct_NegativeStockValue_DomainExceptionInvalidStock(int value)
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 99.99m, value, "Image");
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid stock. Stock is required");
+
+        }
+
+        [Fact]
+        public void CreateProduct_LongImageValue_DomainExceptionLongImage()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 99.99m, 1, "Image Loooooooooooooooooooooooooooooooooooooonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnngggaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$4");
+            action.Should()
+                .Throw<DomainExceptionValidation>()
+                .WithMessage("Invalid image, too long, maximum 250 characters");
+
+        }
+    }
+}
--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -22,7 +22,7 @@ namespace CleanArchMvc.Domain.Entities
 
         public Product(int id, string name, string description, decimal price, int stock, string image)
         {
-            DomainExceptionValidation.When(Id < 0, "Invalid id value");
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
             Id = id;
             ValidadeDomain(name, description, price, stock, image);
         }
@@ -32,7 +32,7 @@ namespace CleanArchMvc.Domain.Entities
             DomainExceptionValidation.When(String.IsNullOrEmpty(name), "Invalid name. Name is required");
             DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
             DomainExceptionValidation.When(String.IsNullOrEmpty(description), "Invalid description. Description is required");
-            DomainExceptionValidation.When(description.Length < 5, "Invalid description, too short, minimum 3 characters");
+            DomainExceptionValidation.When(description.Length < 5, "Invalid description, too short, minimum 5 characters");
             DomainExceptionValidation.When(price < 0, "Invalid price. Price is required");
             DomainExceptionValidation.When(stock < 0, "Invalid stock. Stock is required");
             DomainExceptionValidation.When(image.Length > 250, "Invalid image, too long, maximum 250 characters");
