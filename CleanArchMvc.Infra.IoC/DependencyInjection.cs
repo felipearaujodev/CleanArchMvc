6cac79c162e28df0cff5db1c1b02537e789db778
Felipe Araujo <comunicar.felipearaujo@gmail.com>
2024-02-05 16:03:43 -0300

    adicionando serviço de login, registro e logout

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,9 +1,12 @@
 ﻿using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Application.Mappings;
 using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
 using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,6 +27,13 @@ namespace CleanArchMvc.Infra.IoC
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
                 b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+                     options.AccessDeniedPath = "/Account/Login");
+
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
@@ -31,6 +41,9 @@ namespace CleanArchMvc.Infra.IoC
 
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedRolerUserInitital, SeedRoleUserInitial>();
 
             var myHandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(myHandlers));
