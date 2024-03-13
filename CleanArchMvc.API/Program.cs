39bc7b6d154824ad882317c608f55cbc4078484e
Felipe Araujo <comunicar.felipearaujo@gmail.com>
2024-03-13 14:57:24 -0300

    autenticação JWT

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(x =>
+x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 builder.Services.AddInfrastructureAPI(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
